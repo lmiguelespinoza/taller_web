03ade5d1ec11468b2021260ef36e2c27416e5568
lmiguelespinoza <lmiguelespinoza@gmail.com>
2012-09-15 20:52:24 -0500

    Adicionando - reloader - para refrescar con F5

--- bar00.rb
+++ bar00.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
-	"Ubicacion de bares en Lima    Donde quedamos "
+	"Ubicacion de bares en Lima    -    Donde quedamos "
 end
